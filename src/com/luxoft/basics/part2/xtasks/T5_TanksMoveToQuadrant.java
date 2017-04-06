07c0c6d7398650a5933ec8b287af4fd4d009dfaa
tanjatko <lyisa@meta.ua>
2017-04-06 12:48:59 +0300

    test

--- src/com/luxoft/basics/part2/xtasks/T5_TanksMoveToQuadrant.java
+++ src/com/luxoft/basics/part2/xtasks/T5_TanksMoveToQuadrant.java
@@ -10,30 +10,91 @@ public class T5_TanksMoveToQuadrant extends JPanel
 
     void runTheGame()
     {
-        moveToQuadrant("h3");
+
+        moveToQuadrant("F9");
     }
 
     /**
      * One method call should smoothly move the tank one quadrant according to given direction.
      *
-     * @param direction can be 1 - up, 2 - right, 3 - down, 4 - right
+     * @param \direction can be 1 - up, 2 - right, 3 - down, 4 - left
      */
-    void move(int direction)
-    {
-        // TODO SHOULD BE IMPLEMENTED IN CLASS
+
+    void moveToQuadrant(String quadrant) {
+
+        int desiredNumberOfQuadro = (int)quadrant.charAt(1);
+        char desiredCharOfQuadro = quadrant.charAt(0);
+
+        while ((int)(getTankQuadrant(tankX,tankY).charAt(1))!=desiredNumberOfQuadro)
+        {
+            int upOrDown = getTankQuadrant(tankX,tankY).charAt(1) - desiredNumberOfQuadro;
+            System.out.println(upOrDown);
+
+            if (upOrDown < 0) {
+                slowMove('+', 'Y'); //down
+            } else
+                slowMove('-', 'Y'); //up
+        }
+
+        while ((int)(getTankQuadrant(tankX,tankY).charAt(0))!=desiredCharOfQuadro)
+        {
+            System.out.println(getTankQuadrant(tankX,tankY).charAt(0)-desiredCharOfQuadro);
+            int leftOrRight = getTankQuadrant(tankX,tankY).charAt(0)-desiredCharOfQuadro;
+
+            if (leftOrRight<0)
+            {
+                slowMove('+', 'X');
+            }
+            else
+                slowMove('-', 'X');
+        }
+
     }
 
-    void moveToQuadrant(String quadrant)
-    {
-        // TODO YOUR CODE HERE
+    void move(int direction) {
+        if ((direction == 1) && (tankY != 0))
+            slowMove('-','Y');
+        else if ((direction == 2) && (tankX != 512))
+            slowMove('+', 'X');
+        else if ((direction == 3) && (tankY != 512))
+            slowMove('+', 'Y');
+        else if ((direction == 4) && (tankX != 0))
+            slowMove('-','X');
+        sleep(70);
     }
 
+    void slowMove(char dir, char coordinate) {
+
+        int counter = 0;
+        while (counter < 64) {
+            repaint();
+            if ((dir == '+')&&(coordinate=='X'))
+                tankX++;
+            else if ((dir == '-')&&(coordinate=='X'))
+                tankX--;
+            else if  ((dir == '+')&&(coordinate=='Y'))
+                tankY++;
+            else if  ((dir == '-')&&(coordinate=='Y'))
+                tankY--;
+            counter++;
+            sleep(8);
+        }
+    }
+
+    String getTankQuadrant(int x, int y)
+    {
+        char res1 = (char)(((x+64)/64)+64);
+        int res2 = (y+64)/64;
+        String res = res1+""+res2;
+
+        return res;
+    }
 
     // Magic bellow. Do not worry about this now, you will understand everything in this course.
     // Please concentrate on your tasks only.
 
-    final int BF_WIDTH = 576;
-    final int BF_HEIGHT = 576;
+    final int BF_WIDTH = 590;
+    final int BF_HEIGHT = 590;
 
     public static void main(String[] args) throws Exception
     {
